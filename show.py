045624701c8cbfa14ccf11bf185180506fab5879
Erik Bernhardsson <erikbern@spotify.com>
2015-12-09 23:24:09 -0500

    show same font 10x

--- show.py
+++ show.py
@@ -5,8 +5,8 @@ f = h5py.File('fonts.hdf5', 'r')
 data = f['fonts']
 print data.shape
 
-for i in xrange(10):
-    i = random.randint(0, data.shape[0]-1)
+i = random.randint(0, data.shape[0]-1)
+for z in xrange(10):
     j = random.randint(0, data.shape[1]-1)
     m = data[i][j]
     pyplot.matshow(m, cmap='gray')
