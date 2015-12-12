92fc686350530bb732604742b4f423a5fc2baad0
Erik Bernhardsson <mail@erikbern.com>
2015-12-11 19:26:31 -0500

    do crossvalidation

--- model.py
+++ model.py
@@ -10,29 +10,31 @@ class Model(object):
         self.input_char = T.matrix('input_char')
         self.target = T.matrix('target')
         
-        input_font = lasagne.layers.InputLayer(shape=(None, n), input_var=self.input_font)
-        input_char = lasagne.layers.InputLayer(shape=(None, k), input_var=self.input_char)
-        input_font_bottleneck = lasagne.layers.DenseLayer(input_font, 256)
-        input_char_bottleneck = lasagne.layers.DenseLayer(input_char, 64)
-        network = lasagne.layers.ConcatLayer([input_font_bottleneck, input_char_bottleneck])
-        network = lasagne.layers.DropoutLayer(network)
+        input_font = lasagne.layers.InputLayer(shape=(None, n), input_var=self.input_font, name='input_font')
+        input_char = lasagne.layers.InputLayer(shape=(None, k), input_var=self.input_char, name='input_char')
+        input_font_bottleneck = lasagne.layers.DenseLayer(input_font, 256, name='input_font_bottleneck')
+        input_char_bottleneck = lasagne.layers.DenseLayer(input_char, 64, name='input_char_bottleneck')
+        network = lasagne.layers.ConcatLayer([input_font_bottleneck, input_char_bottleneck], name='input_concat')
+        # network = lasagne.layers.DropoutLayer(network, name='input_concat_dropout')
         for i in xrange(4):
-            network = lasagne.layers.DenseLayer(network, 2048)
+            network = lasagne.layers.DenseLayer(network, 2048, name='dense_%d' % i)
 
-        network = lasagne.layers.DenseLayer(network, wh, nonlinearity=lasagne.nonlinearities.sigmoid)
+        network = lasagne.layers.DenseLayer(network, wh, nonlinearity=lasagne.nonlinearities.sigmoid, name='output_sigmoid')
         self.network = network
         self.prediction_train = lasagne.layers.get_output(network)
         self.prediction = lasagne.layers.get_output(network, deterministic=True)
         print self.prediction.dtype
 
-    def get_train_fn(self, lambd=1e-7):
+    def get_train_fn(self, lambd=1e-7, updates=True):
         print 'compiling training fn'
         loss = lasagne.objectives.squared_error(self.prediction_train, self.target).mean()
         reg = lasagne.regularization.regularize_network_params(self.network, lasagne.regularization.l2) * lambd
         params = lasagne.layers.get_all_params(self.network, trainable=True)
-        updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=lasagne.utils.floatX(1.0), momentum=lasagne.utils.floatX(0.9))
+        kwargs = {}
+        if updates:
+            kwargs['updates'] = lasagne.updates.nesterov_momentum(loss, params, learning_rate=lasagne.utils.floatX(1.0), momentum=lasagne.utils.floatX(0.9))
 
-        return theano.function([self.input_font, self.input_char, self.target], [loss, reg], updates=updates)
+        return theano.function([self.input_font, self.input_char, self.target], [loss, reg], **kwargs)
 
     def get_run_fn(self):
         return theano.function([self.input_font, self.input_char], self.prediction)
--- train_model.py
+++ train_model.py
@@ -7,6 +7,8 @@ import pickle
 import os
 import wget
 import model
+from sklearn import cross_validation
+import sys
 
 if not os.path.exists('fonts.hdf5'):
     wget.download('https://s3.amazonaws.com/erikbern/fonts.hdf5')
@@ -16,15 +18,21 @@ data = f['fonts']
 n, k = data.shape[0], data.shape[1]
 wh = data.shape[2] * data.shape[3]
 
+dataset = []
+for i in xrange(n):
+    for j in xrange(k):
+        dataset.append((i, j))
 
-def iterate_minibatches(batch_size=128):
-    while True:
+train_set, test_set = cross_validation.train_test_split(dataset, test_size=2000, random_state=0)
+
+def iterate_minibatches(dataset, batch_size=128):
+    random.shuffle(dataset)
+    for offset in xrange(0, len(dataset) - batch_size, batch_size):
         batch_fonts = numpy.zeros((batch_size, n), dtype=theano.config.floatX)
         batch_chars = numpy.zeros((batch_size, k), dtype=theano.config.floatX)
         batch_ds = numpy.zeros((batch_size, wh), dtype=theano.config.floatX)
         for z in xrange(batch_size):
-            i = random.randint(0, n-1)
-            j = random.randint(0, k-1)
+            i, j = dataset[offset + z]
             batch_fonts[z][i] = 1
             batch_chars[z][j] = 1
             batch_ds[z] = data[i][j].flatten() * 1. / 255
@@ -33,17 +41,31 @@ def iterate_minibatches(batch_size=128):
 
 model = model.Model(n, k, wh)
 model.try_load()
-train_fn = model.get_train_fn()
+train_fn = model.get_train_fn(updates=True)
+test_fn = model.get_train_fn(updates=False)
 run_fn = model.get_run_fn()
 
 print 'training...'
-for input_font, input_char, output in iterate_minibatches():
-    loss, reg = train_fn(input_font, input_char, output)
-    print '%.9f %.9f' % (float(loss), float(reg))
+epoch = 0
+while True:
+    print 'epoch', epoch
+    for input_font, input_char, output in iterate_minibatches(train_set):
+        loss, reg = train_fn(input_font, input_char, output)
+        sys.stdout.write('Train perf: %.9f %.9f\r' % (float(loss), float(reg)))
+        sys.stdout.flush()
+        break
+    sys.stdout.write('\n')
+    epoch += 1
+    total_loss, total_reg = 0, 0
+    for input_Font, input_char, output in iterate_minibatches(test_set):
+        loss, reg = test_fn(input_font, input_char, output)
+        total_loss += loss
+        total_reg += reg
+    sys.stdout.write('Test perf: %.9f %.9f\n' % (float(loss), float(reg)))
+    model.save()
+    
+    continue
     real = output.reshape(output.shape[0], 64, 64)
-    if random.random() < 0.001:
-        model.save()
-
     pred = run_fn(input_font, input_char).reshape((output.shape[0], 64, 64))
     f, (ax1, ax2) = pyplot.subplots(1, 2)
     ax1.matshow(real[0], cmap='gray')
