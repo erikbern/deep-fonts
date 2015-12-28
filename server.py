d37117dc24bc81f4177393ddf1ddfd18d1076bde
Erik Bernhardsson <erikbern@spotify.com>
2015-12-28 12:12:09 -0600

    simple server to generate fonts

---
+++ server.py
@@ -0,0 +1,44 @@
+import flask
+import model
+import numpy
+import PIL, PIL.Image, PIL.PngImagePlugin
+import StringIO
+import jinja2
+
+model = model.Model(artificial_font=True)
+model.try_load()
+run_fn = model.get_run_fn()
+
+app = flask.Flask('fonts')
+
+env = jinja2.Environment(loader=jinja2.FileSystemLoader('static'))
+    
+@app.route('/')
+def root():
+    template = env.get_template('index.html')
+    return template.render(d=model.d)
+
+
+@app.route('/font.png')
+def font():
+    input_i = numpy.array(map(float, flask.request.query_string.split(',')))
+    input_i = numpy.array([input_i for z in xrange(62)])
+    input_j = numpy.array(range(62), dtype=numpy.int32)
+    pred = run_fn(input_i, input_j).reshape((62, 64, 64))
+
+    img = PIL.Image.new('L', (8 * 64, 8 * 64), 255)
+    for z in xrange(62):
+        x, y = z % 8, z // 8
+        img_char = PIL.Image.fromarray(numpy.uint8(((1.0 - pred[z]) * 255)))
+        img.paste(img_char, (x * 64, y * 64))
+
+    s = StringIO.StringIO()
+    img.save(s, format='PNG')
+    return flask.Response(response=s.getvalue(), mimetype='image/png')
+
+
+def main():
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    main()
