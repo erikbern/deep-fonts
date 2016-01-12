8386e1192577522e0508a84bdf8ef3a57fceef87
Erik Bernhardsson <erikbern@spotify.com>
2016-01-11 22:01:09 -0500

    removed crap

--- generate_fonts.py
+++ generate_fonts.py
@@ -1,22 +1,10 @@
-import h5py
 import random
 import numpy
 import theano
-import pickle
-import os
-import wget
 import PIL, PIL.Image
 import model
 
-if not os.path.exists('fonts.hdf5'):
-    wget.download('https://s3.amazonaws.com/erikbern/fonts.hdf5')
-
-f = h5py.File('fonts.hdf5', 'r')
-data = f['fonts']
-n, k = data.shape[0], data.shape[1]
-wh = data.shape[2] * data.shape[3]
-
-model = model.Model(n, k, wh, artificial_font=True)
+model = model.Model(artificial_font=True)
 model.try_load()
 run_fn = model.get_run_fn()
 W = model.get_font_embeddings()
@@ -31,10 +19,9 @@ def generate_input():
         b = generate_font()
         for p in numpy.linspace(0, 1, 10):
             print p
-            batch_is = numpy.zeros((k, model.d), dtype=theano.config.floatX)
-            batch_js = numpy.zeros((k,), dtype=numpy.int32)
-            i = random.randint(0, n-1)
-            for z in xrange(k):
+            batch_is = numpy.zeros((model.k, model.d), dtype=theano.config.floatX)
+            batch_js = numpy.zeros((model.k,), dtype=numpy.int32)
+            for z in xrange(model.k):
                 batch_is[z] = a * (1-p) + b * p
                 batch_js[z] = z
 
@@ -47,7 +34,7 @@ for input_i, input_j in generate_input():
     pred = run_fn(input_i, input_j).reshape((input_i.shape[0], 64, 64))
 
     img = PIL.Image.new('L', (8 * 64, 8 * 64), 255)
-    for z in xrange(k):
+    for z in xrange(model.k):
         x, y = z % 8, z // 8
         img_char = PIL.Image.fromarray(numpy.uint8(((1.0 - pred[z]) * 255)))
         img.paste(img_char, (x * 64, y * 64))
