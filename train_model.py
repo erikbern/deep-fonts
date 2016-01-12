4b883278b8045537f3b2618b8e544982e15cc206
Erik Bernhardsson <erikbern@spotify.com>
2016-01-11 22:40:19 -0500

    smaller batch size, cut learning rate more aggressively

--- train_model.py
+++ train_model.py
@@ -6,7 +6,7 @@ import sys
 import functools
 import time
 
-def iterate_minibatches(dataset, batch_size=2048):
+def iterate_minibatches(dataset, batch_size=512):
     random.shuffle(dataset)
     for offset in xrange(0, len(dataset) - batch_size, batch_size):
         batch_fonts = numpy.zeros((batch_size,), dtype=numpy.int32)
@@ -36,6 +36,7 @@ def iterate_run(dataset, fn, tag):
     sys.stdout.write('\n')
     return total_loss / total_count
 
+
 data = model.get_data()
 n, k = data.shape[0], data.shape[1]
 wh = data.shape[2] * data.shape[3]
@@ -48,18 +49,14 @@ train_set, test_set = model.sets()
 
 print 'training...'
 for learning_rate in [1.0, 0.3, 0.1, 0.03, 0.01]:
-    epoch = 0
-    best_epoch = 0
-    best_loss = float('inf')
+    epoch, last_loss = 0, float('inf')
     while True:
         print 'epoch', epoch, 'learning rate', learning_rate
         train_fn = functools.partial(train_fn_w_learning_rate, learning_rate)
         iterate_run(train_set, train_fn, 'train')
         loss = iterate_run(test_set, test_fn, 'test ')
-        if loss < best_loss:
-            print 'best loss'
-            model.save()
-            best_loss, best_epoch = loss, epoch
-        if epoch - best_epoch > 2:
-            break
+        if loss > last_loss:
+            break # decrease learning rate
+        last_loss = loss
+        model.save()
         epoch += 1
