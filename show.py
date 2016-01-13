aa3290ae4514c54cef552940001afd9f6bbd7c2a
Erik Bernhardsson <erikbern@spotify.com>
2016-01-12 21:30:34 -0500

    blur for show.py :

--- show.py
+++ show.py
@@ -1,5 +1,7 @@
 import h5py, random, numpy
 import PIL, PIL.Image
+from scipy.ndimage import filters
+import random
 
 f = h5py.File('fonts.hdf5', 'r')
 data = f['fonts']
@@ -8,7 +10,9 @@ print data.shape
 i = random.randint(0, data.shape[0]-1)
 for z in xrange(10):
     j = random.randint(0, data.shape[1]-1)
-    img = PIL.Image.fromarray(numpy.uint8(255 - data[i][j]))
+    m = data[i][j]
+    m = filters.gaussian_filter(m, sigma=random.random()*1.0)
+    img = PIL.Image.fromarray(numpy.uint8(255 - m))
     img.show()
 
 
